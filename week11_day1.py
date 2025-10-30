f5d4beb321adfadd1cece2fa641bc7d374e344d2
joaquin0899 <jrflores15@cps.edu>
2025-10-30 13:33:07 +0000

    online

---
+++ week11_day1.py
@@ -0,0 +1,39 @@
+
+print(" Welcome to the Python Review Activity!")
+print("You’ll review variables, strings, numbers, and print formatting.\n")
+
+# Step 1: Create Variables
+# TODO: Replace the values below with your own info
+first_name = "Joaquin"
+age = 17
+favorite_color = "Purple"
+favorite_number = 24
+
+#  Step 2: Practice String Operations
+# 1. Print your name in uppercase
+print(first_name.upper())
+
+# 2. Print how many letters are in your name
+print("Length of name:", len(first_name))
+
+# 3. Combine your name and favorite color into one message
+print("My name is", (first_name), "and my favorite color is", (favorite_color))
+
+#  Step 3: Math Practice
+# Use arithmetic operators with your favorite number
+print(24 + 24)
+print(24-24)
+print(24*24)
+print("24 divided by 2 is", 24/2)
+#  Step 4: User Input Practic
+# Ask the user two questions and combine answers
+
+input("What is your name ")
+
+print("Your name is: ", (input))
+
+
+# ⚙️ Step 5: Final Challenge (combine it all)
+# Use math and strings together
+
+
